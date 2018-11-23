0c0e73f6796881c6ac039ebd9207e4d4e5f05eac
Ivan Burnaev <ivan.biirus@gmail.com>
2018-11-23 14:01:33 +0300

    fix(symbols): change unique symbols to global ones
    Because of unique symbols it was imposible to use two instances of redux-async-epic. It was
    imposible to check equality. That's why it is changed to Symbol.for() to register symbol in global
    space

--- src/symbols.js
+++ src/symbols.js
@@ -1,5 +1,5 @@
-export const async = Symbol("async");
-export const pending = Symbol("pending");
-export const success = Symbol("success");
-export const error = Symbol("error");
-export const abort = Symbol("abort");
+export const async = Symbol.for("@rae/async");
+export const pending = Symbol.for("@rae/pending");
+export const success = Symbol.for("@rae/success");
+export const error = Symbol.for("@rae/error");
+export const abort = Symbol.for("@rae/abort");
